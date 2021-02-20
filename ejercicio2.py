4dd2f191c85eb27665da82077c16b4ae523593c1
= <=>
2021-02-20 12:16:09 -0700

    Comentando los IFS

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,7 +1,8 @@
 def number_multiplies(number, length):
-    #comprobamos que los argumentos no sean negativos y que si sean ENTEROS
+    #si los argumentos NO SON ENTEROS
     if not isinstance(number,int) or not isinstance(length,int):
         return None, "Error"
+    #si los argumentos SON NEGATIVOS
     if number < 0 or length < 0:
         return None, "Error"
     list_of_multiplies = [number * element for element in range(1, length + 1)]

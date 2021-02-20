4711fd1b2b4d20a1e4134283a80ca0472b8906c9
= <=>
2021-02-20 13:52:05 -0700

    cambios validacion

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,5 @@
-def format_number(number):
-     if not isinstance(number,int):
+def format_number(number, separator):
+    if not isinstance(number,int) or not isinstance(separator,str):
         return None, "Error"
     if number < 0:
         return None, "Error"
@@ -9,10 +9,10 @@ def format_number(number):
     stop_count = 0
     new_string = ""
     #agregamos las posiciones donde se pone el separator en un arreglo
-    for i in range(length_string):
+    for index in range(length_string):
         stop_count += 1
         if stop_count == 3:
-            list_positions.append(i)
+            list_positions.append(index)
             stop_count = 0
     print(list_positions)
     #iteramos con la longitud del string dado para ir agregando caracter por caracter
@@ -20,16 +20,12 @@ def format_number(number):
         #iteramos con la lista de las posiciones donde se agrega el separados
         for position in list_positions:
             if index_string == position:
-                new_string += "."
-        new_string += string[index_string]
+                new_string += separator
+        new_string += string_number[index_string]
     print(new_string)
         
-        
 
 
- 
-
-"1,000,000"
 
 
     

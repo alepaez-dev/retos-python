36ff21e2c801e6f55aa9a5b7eea4fdf8ec107b1d
= <=>
2021-02-20 12:27:43 -0700

    Agregando recursividad

--- ejercicio3.py
+++ ejercicio3.py
@@ -6,3 +6,13 @@ def factorial(number):
         total *= number
         number -= 1 #restamos el numero para interactuar con todos
     return total
+
+##recursividad
+def factorial_recursividad(number):
+    #nuestro stop sera cuando llegue a 1
+    if number > 1:
+        #multiplicamos el numero por el resultado de la misma funcion con un numero antes
+        #example: 7 * (6 * (5 * (4 * (3 * (2 * )))) )
+        return number * factorial_recursividad(number - 1)
+    else:
+        return 1

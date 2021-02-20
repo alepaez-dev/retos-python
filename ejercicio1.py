4e2348c3995e7668f357f3817affa6f07b7b828b
= <=>
2021-02-20 11:29:35 -0700

    cambios de logica

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,11 +1,10 @@
 #Crea una funcion que reciba un numero como argumento y devuelva el largo de este numero
 def number_length(number):
     #comprobamos que no sea negativo y que si sea ENTERO
-    if isinstance(number,int) and number > 0:
-        number_string = str(number) ##convertimos a string
-        count = 0 #contador
-    else:
+    if not isinstance(number,int) or number < 0:
         return None, "Error"
+    number_string = str(number) ##convertimos a string
+    count = 0 #contador
     for character in number_string:
         count += 1; #sumamos 1 al contador por cada caracter del string
     return count;

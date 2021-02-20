fc4c3c760931f436575d6b28508e9aaa86df0086
= <=>
2021-02-20 12:24:11 -0700

    Comentando codigo

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,7 +1,8 @@
 #normal
 def factorial(number):
     total = 1 #si es 1 factorial es 1
+    #multiplicamos desde el numero hasta llegar a 2
     while number > 1:
         total *= number
-        number -= 1 #restamos el numero
+        number -= 1 #restamos el numero para interactuar con todos
     return total

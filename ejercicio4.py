375e118f650f6bb0a0c7fefaed837b3f95f5b8e9
= <=>
2021-02-20 14:08:07 -0700

    cambiando funcion

--- ejercicio4.py
+++ ejercicio4.py
@@ -4,26 +4,25 @@ def format_number(number, separator):
     if number < 0:
         return None, "Error"
     string_number = str(number)
-    list_positions = []
     length_string = len(string_number)
-    stop_count = 0
     new_string = ""
-    #agregamos las posiciones donde se pone el separator en un arreglo
-    for index in range(length_string):
+    stop_count = 0
+    index = length_string - 1
+    #iteramos desde el final hasta el principio
+    while index >= 0 :
         stop_count += 1
-        if stop_count == 3:
-            list_positions.append(index)
+        #cada 3 posiciones agregar el separator
+        if stop_count == 4:
+            new_string += separator
             stop_count = 0
-    print(list_positions)
-    #iteramos con la longitud del string dado para ir agregando caracter por caracter
-    for index_string in range(length_string):
-        #iteramos con la lista de las posiciones donde se agrega el separados
-        for position in list_positions:
-            if index_string == position:
-                new_string += separator
-        new_string += string_number[index_string]
+        #sino simplemente agregar el caracter que es
+        else:
+            new_string += string_number[index]
+            index -=1
+    #le damos reverse
+    new_string = new_string[::-1]
     print(new_string)
-        
+
 
 
 

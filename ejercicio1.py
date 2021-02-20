a9569af4900f838355135d9e8c87cc18db9b6016
= <=>
2021-02-20 11:36:56 -0700

    Separe los ifs

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,9 @@
 #Crea una funcion que reciba un numero como argumento y devuelva el largo de este numero
 def number_length(number):
     #comprobamos que no sea negativo y que si sea ENTERO
-    if not isinstance(number,int) or number < 0:
+    if not isinstance(number,int):
+        return None, "Error"
+    if number < 0:
         return None, "Error"
     number_string = str(number) ##convertimos a string
     count = 0 #contador

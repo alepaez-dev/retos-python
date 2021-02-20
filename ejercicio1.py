62b2241fa2edbce16c7111c33ea0e73ce410c3e1
= <=>
2021-02-20 10:52:10 -0700

    Primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+#Crea una funcion que reciba un numero como argumento y devuelva el largo de este numero
+
+def number_length(number):
+    #comprobamos que no sea negativo
+    if number > 0:
+        number_string = str(number) ##convertimos a string
+        count = 0 #contador
+        for character in number_string:
+            count += 1; #sumamos 1 al contador por cada caracter del string
+        return count
+    else:
+        return None, "No se puede hacer en numeros negativos"
+    
